cf973a3b7208aca01c1ca2749a56110e82c09036
vignesh <vignesh@swethas-macbook-pro.local>
2020-07-01 22:18:09 -0700

    Initial Commit

---
+++ FirstSwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstSwiftApp
+//
+//  Created by vignesh on 7/1/20.
+//  Copyright © 2020 AmVi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
